c74863a6ac1dd960bae0b47754beaefd22a9fe79
Hwang Jiwon <cutty82432@gmail.com>
2024-11-12 11:59:26 +0900

    feat: 게시글 상세 조회(댓글 포함) 기능 구현

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,15 @@
 // TODO: router 합치기
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getPosts, getAuthorProfile } from '../controllers/posts-controller.js';
+import { getPosts, getAuthorProfile, getPostDetail, getComments } from '../controllers/posts-controller.js';
 
 const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', getPosts);
+router.get('/:post_id', getPostDetail);
+router.get('/:post_id/comments', getComments);
 router.get('/author/:author_id', getAuthorProfile);
 
 
