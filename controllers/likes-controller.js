b24b25b5e8cf7e1fb8183c33a0951d40aa7723bd
JiwonHwang <cutty82432@gmail.com>
2024-11-18 15:48:31 +0900

    feat: 게시글 id 유효성 검증 추가(#6)

--- controllers/likes-controller.js
+++ controllers/likes-controller.js
@@ -1,10 +1,11 @@
 import Like from '../models/Like.js';
+import Post from '../models/Post.js';
 
 function createLike(req, res) {
     const post_id = parseInt(req.params.post_id);
     const user_id = req.session.userId
     if (!user_id) {
-        return res.status(401).json({ message: "권한 없음" })
+        return res.status(401).json({ message: "로그인이 필요합니다." })
     }
 
     try {
@@ -20,7 +21,14 @@ function updateLike(req, res) {
     const post_id = parseInt(req.params.post_id);
     const user_id = req.session.userId
     if (!user_id) {
-        return res.status(401).json({ message: "권한 없음" })
+        return res.status(401).json({ message: "로그인이 필요합니다" });
+    }
+
+    // 유효성 검증 - 없는 게시글에 좋아요
+    const posts = Post.loadPosts();
+    const isValidPost = posts.some(post => post.id == post_id);
+    if (!isValidPost) {
+        return res.status(404).json({ message: '존재하지 않는 게시글입니다' });
     }
 
     try {
