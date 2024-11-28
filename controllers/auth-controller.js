7f34fcbd2d5d13e7aee9d8a4d20accdd6affd910
JiwonHwang <cutty82432@gmail.com>
2024-11-29 00:08:45 +0900

    feat: 비밀번호 암호화 구현(#13)

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,13 +1,13 @@
 import User from '../models/User.js';
 
-function loginUser(req, res) {
+async function loginUser(req, res) {
     const { email, password } = req.body;
 
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     try{
-        const user = User.login(email, password);
+        const user = await User.login(email, password);
         if(!user){
             res.status(401).json({message: "회원 정보가 일치하지 않습니다."})
         }
@@ -27,11 +27,11 @@ function loginUser(req, res) {
     }
 }
 
-function signupUser(req, res) {
+async function signupUser(req, res) {
     const { email, pwd, profile_img, pwd2, username } = req.body;
     try {
         const newUser = new User(email, pwd, username, profile_img, new Date(), new Date());
-        newUser.addUser();
+        await newUser.addUser();
 
         return res.status(200).send('회원가입 성공!');
     } catch(error) {
