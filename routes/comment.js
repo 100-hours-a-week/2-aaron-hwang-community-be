5dfec6e37ad322e6c1d235caaaa9893c118a0f3f
JiwonHwang <cutty82432@gmail.com>
2024-11-29 00:51:48 +0900

    refactor: body-parser 코드 삭제

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,7 @@
-// TODO: router 합치기
 import express from 'express';
-import bodyParser from 'body-parser';
 import { createComment, updateComment, deleteComment } from '../controllers/comments-controller.js';
 
 const router = express.Router();
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/:post_id', createComment);
 router.patch('/:comment_id', updateComment);
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,8 @@
-// TODO: router 합치기
 import express from 'express';
-import bodyParser from 'body-parser';
 import { getPosts, getPostDetail, createPost, updatePost, deletePost } from '../controllers/posts-controller.js';
 import { updateLike } from '../controllers/likes-controller.js';
 
 const router = express.Router();
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', getPosts);
 router.get('/:post_id', getPostDetail);
@@ -16,7 +12,5 @@ router.delete('/:post_id', deletePost);
 
 // 좋아요 기능
 router.post('/:post_id/likes', updateLike);
-
-// TODO: 작성자 url 수정
 
 export default router;
