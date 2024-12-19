8384b16b8f74dc006ef358b260f46d58c62a75e2
JiwonHwang <cutty82432@gmail.com>
2024-12-19 15:26:58 +0900

    fix: Cors세팅

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,9 @@
 import cors from 'cors';
 
 const options = {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://54.180.235.48:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization','Cache-Control'],
     credentials: true,
 };
 
