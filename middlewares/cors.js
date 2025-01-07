7ffc47c0750bb31a36b6a10035a3ebd671aa5f44
JiwonHwang <cutty82432@gmail.com>
2025-01-07 16:52:12 +0900

    ec2 ip 변경 cors 재설정

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const options = {
-    origin: ['http://localhost:3000', 'http://54.180.235.48:3000'],
+    origin: ['http://localhost:3000', 'http://43.201.21.191:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization','Cache-Control'],
     credentials: true,
