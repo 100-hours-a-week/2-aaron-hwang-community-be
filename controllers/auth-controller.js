5b677bfa7ef1c342f786ffda2adfd23e3ec08add
JiwonHwang <cutty82432@gmail.com>
2025-01-07 17:25:52 +0900

    fix: 회원가입 이후 로그인 페이지로 이동

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -62,25 +62,25 @@ async function signupUser(req, res) {
             console.log('controller signup error')
             return res.status(500).json({ message: '회원가입 중 오류가 발생했습니다.' });
         }
+        /* 
         // 세션 설정
         req.session.userId = createdUser.id;
         req.session.email = createdUser.email;
         req.session.profile_img = loadBinaryFile(createdUser.profile_img.split('\\').pop()).toString('base64');
         req.session.username = createdUser.username;
-
+ */
         return res.status(201).json({ message: '회원가입 성공!', data: createdUser });
     } catch(error) {
         return res.status(500).json({ error: error.message });
     }
 }
 
-function logout(req, res) {
-    req.session.destroy(err => {
-        if (err) {
-          return res.status(500).json({ message: "로그아웃 실패" });
-        }
-        
-        return res.json({ message: "로그아웃 성공" });
+async function logout(req, res) {
+    await req.session.destroy(err => {
+        if (err) return res.status(500).json({ message: "서버사이드 세션 삭제 오류" });
+            
+        res.clearCookie('connect.sid');
+        res.status(200).json({ message: '로그아웃이 성공적으로 처리되었습니다.' });
     });
 }
 
